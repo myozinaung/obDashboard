b3883dcccd576f74c114c7b63477e9a0d5fb406f
Myo Zin Anng <mgmyozinaung@gmail.com>
2021-06-11 04:21:07 +0900

    Everything transfered from old project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,15 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
+import { TopNavbarComponent } from './top-navbar/top-navbar.component';
+import { InstrumentPanelComponent } from './instrument-panel/instrument-panel.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent, TopNavbarComponent, InstrumentPanelComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -17,10 +17,11 @@ import { environment } from '../environments/environment';
       enabled: environment.production,
       // Register the ServiceWorker as soon as the app is stable
       // or after 30 seconds (whichever comes first).
-      registrationStrategy: 'registerWhenStable:30000'
-    })
+      registrationStrategy: 'registerWhenStable:30000',
+    }),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/instrument-panel/instrument-panel.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+import 'openbridge-web-components';
+
+@Component({
+  selector: 'app-instrument-panel',
+  templateUrl: './instrument-panel.component.html',
+  styleUrls: ['./instrument-panel.component.scss'],
+})
+export class InstrumentPanelComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
