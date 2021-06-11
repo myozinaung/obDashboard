813ef58f825824ffde93241e641b511edb195809
Myo Zin Anng <mgmyozinaung@gmail.com>
2021-06-11 22:42:18 +0900

    Thing switching function added

--- src/app/dimming-menu/dimming-menu.component.ts
+++ src/app/dimming-menu/dimming-menu.component.ts
@@ -3,13 +3,14 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-dimming-menu',
   templateUrl: './dimming-menu.component.html',
-  styleUrls: ['./dimming-menu.component.scss']
+  styleUrls: ['./dimming-menu.component.scss'],
 })
 export class DimmingMenuComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  setTheme(themeName: string) {
+    document.documentElement.setAttribute('theme', themeName);
   }
-
 }
